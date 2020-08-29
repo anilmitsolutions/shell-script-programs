9b49533a52c909f1a52aa543b8b1830f62408164
Cloud User <ec2-user@ip-172-31-42-2.ap-south-1.compute.internal>
2020-08-29 14:18:01 +0000

    update check cherry-pick

--- cleanlogfiles.sh
+++ cleanlogfiles.sh
@@ -3,8 +3,12 @@ LOG_DIR=/var/log
 cd $LOG_DIR
 #print messages
 #check
+Hello this is to check
+wether file merging is workin or not
 cat /dev/null > messages
 cat /dev/null > wtmp
 #check the working of cherry-pick
 #wether it merges or not
 echo "Logs cleaned up."
+check!!
+check!!
